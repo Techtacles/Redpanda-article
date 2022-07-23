e7c56d8f380f0fa3871fcfd09a1249bca5c7b04f
Techtacles <emmanueloffisong2002@yahoo.com>
2022-07-23 14:53:39 +0100

    Second commit

--- Redpanda/build.sbt
+++ Redpanda/build.sbt
@@ -7,5 +7,7 @@ libraryDependencies++=Seq("org.apache.spark" %% "spark-sql" % "3.2.1",
   "org.apache.spark" %% "spark-streaming" % "3.2.1",
   "org.apache.kafka"%"kafka-clients"%"3.1.0",
   "org.apache.spark" %% "spark-core" % "3.2.1",
-  "org.apache.spark"%"spark-sql-kafka-0-10_2.13"%"3.2.1"
+  "org.apache.spark"%"spark-sql-kafka-0-10_2.13"%"3.2.1",
+  "com.google.cloud.spark" %% "spark-bigquery-with-dependencies" % "0.26.0"
+
 )
--- Redpanda/src/main/scala/RedpandaSpark.scala
+++ Redpanda/src/main/scala/RedpandaSpark.scala
@@ -1,51 +1,39 @@
 
-import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
-import org.apache.spark.sql.functions.from_json
-
-import scala.List
+import org.apache.spark.sql.SparkSession
+import com.google.cloud.spark.bigquery._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 object RedpandaSpark {
-  import org.apache.spark.sql.SparkSession
-  import org.apache.spark.sql.types._
-  import org.apache.spark.sql.functions._
-
-
-
-
-
   def main(args:Array[String]):Unit= {
     val spark = SparkSession
       .builder
       .appName("Redpanda Stream")
       .config("spark.master", "local")
       .getOrCreate()
-    import spark.implicits._
+    spark.conf.set("viewsEnabled","true")
+    spark.conf.set("credentialsFile","<path-to-json-key-file>")
+    spark.conf.set("materializationDataset","sensor_data")
 
 
     val redpanda_read = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "sensor_stream")
+      .option("subscribe", "sensor_stream").option("startingOffsets","earliest")
       .load()
 
 
     val sensor_data=redpanda_read.selectExpr( "CAST(value AS STRING)")
-    val stream_1=sensor_data.withColumn("value", split(col("value"), ",")).select(col("value")(0).as("id"),col("value")(3).as("temperature")
+    val stream_1=sensor_data.withColumn("value", split(col("value"), ","))
+      .select(col("value")(0).as("id"),col("value")(3).as("temperature")
     )
-    val stream_2=sensor_data.withColumn("value",split(col("value"),",")).select(col("value")(0).as("id"),col("value")(3).as("temperature"))
+    val stream_2=sensor_data.withColumn("value",split(col("value"),","))
+      .select(col("value")(0).as("id"),col("value")(3).as("temperature"))
+
     val joined_stream=stream_1.union(stream_2)
-    /*
-        val neww=agg.withColumn("id",split(col("id"),",")).select(col("id").getItem(0))
-    */
-    /*val redpanda_write = sensor_data
-      .writeStream.format("csv")
-      .option("path", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/output").outputMode("append")
-      .option("checkpointLocation", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/checkpoint")
-      .start().awaitTermination()*/
-    val to_write=joined_stream.writeStream.format("csv")
-      .option("path", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/output").outputMode("append")
-      .option("checkpointLocation", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/checkpoint").start()
-      .awaitTermination()
+
+    val redpanda_write=joined_stream.write.format("bigquery")
+      .option("table", "sensor_data.sensor_table").save()
 
   }
 }
